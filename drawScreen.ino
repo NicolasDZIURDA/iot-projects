dfbe9eba204f77c46fcaea76f8fc52a818d16f97
tomasarn <37652369+tomasarn@users.noreply.github.com>
2019-03-13 17:29:25 +0100

    Add files via upload

---
+++ drawScreen.ino
@@ -0,0 +1,22 @@
+ void  drawScreen(byte buffer2[])
+ { 
+   // Turn on each row in series
+    for (byte i = 0; i < 8; i++)        // count next row
+     {
+        digitalWrite(rows[i], HIGH);    //initiate whole row
+        for (byte a = 0; a < 8; a++)    // count next row
+        {
+          // if You set (~buffer2[i] >> a) then You will have positive
+          digitalWrite(col[a], (buffer2[i] >> a) & 0x01); // initiate whole column
+          
+          delayMicroseconds(100);       // uncoment deley for diferent speed of display
+          //delayMicroseconds(1000);
+          //delay(10);
+          //delay(100);
+          
+          digitalWrite(col[a], 1);      // reset whole column
+        }
+        digitalWrite(rows[i], LOW);     // reset whole row
+        // otherwise last row will intersect with next row
+    }
+}
