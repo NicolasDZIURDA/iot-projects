3ad6a13fb271ed6aefe82b62002c75e6ffebeff1
sMesle <steve.demeulemeester@efrei.net>
2019-04-17 12:00:43 +0200

    Update finalversionmemory.ino

--- Src/finalversion/finalversionmemory.ino
+++ Src/finalversion/finalversionmemory.ino
@@ -115,7 +115,7 @@ void loop() {
         drawScreen(N);
       }
     }
-  // This could be rewritten to not use a delay, which would make it appear brighter 
+  
 else if (timeCount <  240) 
 {
 drawScreen(ALL);
@@ -132,23 +132,19 @@ timeCount = 0;
 }
 void  drawScreen(byte buffer2[])
  { 
-   // Turn on each row in series
-    for (byte i = 0; i < 8; i++)        // count next row
+  
+    for (byte i = 0; i < 8; i++)    
      {
-        digitalWrite(rows[i], HIGH);    //initiate whole row
-        for (byte a = 0; a < 8; a++)    // count next row
+        digitalWrite(rows[i], HIGH);   
+        for (byte a = 0; a < 8; a++)  
         {
-          // if You set (~buffer2[i] >> a) then You will have positive
-          digitalWrite(col[a], (~buffer2[i] >> a) & 0x01); // initiate whole column
           
-          delayMicroseconds(100);       // uncoment deley for diferent speed of display
-          //delayMicroseconds(1000);
-          //delay(10);
-          //delay(100);
+          digitalWrite(col[a], (~buffer2[i] >> a) & 0x01);
           
-          digitalWrite(col[a], 1);      // reset whole column
+          delayMicroseconds(100);
+          
+          digitalWrite(col[a], 1);     
         }
-        digitalWrite(rows[i], LOW);     // reset whole row
-        // otherwise last row will intersect with next row
+        digitalWrite(rows[i], LOW);    
     }
 }
